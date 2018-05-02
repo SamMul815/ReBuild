490fd94fb1654237e1ce4b8d25423bf089ba78c9
SamMul <815youngmin@naver.com>
2018-05-02 20:02:21 +0900

    깔끔이들

---
+++ Assets/Bullet/BulletBase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBase : MonoBehaviour {
+
+    private float moveSpeed;
+    private float moveTime;
+    private Vector3 basePosition;
+    private Transform baseTarget;
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Assets/Manager/AudioSound.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioSound : MonoBehaviour {
+
+    AudioSource audioSource;
+    bool isPlay = false;
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void SetClip(AudioClip _audioClip)
+    {
+        audioSource.clip = _audioClip;
+    }
+
+    public void Play()
+    {
+        audioSource.Play();
+        isPlay = true;
+    }
+
+    public void LoopOn()
+    {
+        audioSource.loop = true;
+    }
+
+    private void Update()
+    {
+        if(!audioSource.isPlaying && isPlay)
+        {
+            SoundManager.Instance.AddEmptyAudioSourceObject(this.gameObject);
+        }
+    }
+
+    public void Reset()
+    {
+        isPlay = false;
+        audioSource.Stop();
+        audioSource.loop = false;
+    }
+}
